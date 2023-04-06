7d6f4afba219d5cc53680552aee0c93d96853765
kurtasov <kurtasov@users.noreply.github.com>
2023-04-06 17:39:41 +0300

    Передача slug в URL

--- proj_maths/views.py
+++ proj_maths/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.cache import cache
 from . import terms_work
+import random
 
 
 def index(request):
@@ -11,6 +12,12 @@ def terms_list(request):
     terms = terms_work.get_terms_for_table()
     return render(request, "term_list.html", context={"terms": terms})
 
+
+def terms_list_new(request, slug):
+    if slug == 'random':
+        terms = terms_work.get_terms_for_table()
+        terms = random.choice(terms)
+    return render(request, "term_list.html", context={"terms": terms, "slug": slug})
 
 def add_term(request):
     return render(request, "term_add.html")
